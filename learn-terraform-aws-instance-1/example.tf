9ea1c6a6bdd0c250ad1e7548ad5fec288f3823f2
Danesh Manoharan <danesh.manoharan@gmail.com>
2020-10-05 15:36:26 -0500

    Outputs

--- learn-terraform-aws-instance-1/example.tf
+++ learn-terraform-aws-instance-1/example.tf
@@ -16,3 +16,8 @@ resource "aws_instance" "example" {
   ami           = var.amis[var.region]
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
