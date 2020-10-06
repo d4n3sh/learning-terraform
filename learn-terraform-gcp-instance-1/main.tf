b0f45d50ee8999c12ff0a54d96b09baf30f45a5e
Danesh Manoharan <danesh.manoharan@gmail.com>
2020-10-05 21:38:18 -0500

    Depends_on

--- learn-terraform-gcp-instance-1/main.tf
+++ learn-terraform-gcp-instance-1/main.tf
@@ -42,3 +42,37 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "d4n3sh-11"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
