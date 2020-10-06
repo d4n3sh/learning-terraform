176fab01bd1c89c4646691e96b2ba5055be2ad17
Danesh Manoharan <danesh.manoharan@gmail.com>
2020-10-05 22:02:34 -0500

    variables

---
+++ learn-terraform-gcp-instance-1/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "terraform-284122"
+}
+
+variable "credentials_file" {
+  default = "../keys/terraform-sa.json"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
